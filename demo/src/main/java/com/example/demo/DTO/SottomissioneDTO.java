e66ee0945f98d56dad031f578e3e1043b0439e80
enric <enrico.privitera@studenti.unicam.it>
2026-01-14 18:50:07 +0100

    Hackathon Service &co

---
+++ demo/src/main/java/com/example/demo/DTO/SottomissioneDTO.java
@@ -0,0 +1,11 @@
+package com.example.demo.DTO;
+
+import lombok.Data;
+
+@Data
+public class SottomissioniDTO {
+    private Long idTeam;
+    private Long idHackathon;
+    private String link;
+    private String descrizione;
+}
